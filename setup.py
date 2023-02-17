026b85f3a8bf8ffc22658c94d41e87f1a0121d9e
felihong <echozhu1991@gmail.com>
2023-02-17 17:04:28 +0100

    Add package source distribution

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-import setuptools
+from setuptools import setup, find_packages
 
 
-setuptools.setup(
-    name='custom_toolbox',
+setup(
+    name='toolbox',
     version='0.0.1',
     author='Hong Zhu',
     author_email='zhuhong@microsoft.com',
     description='Example customized package',
-    url='https://github.com/felihong/Azure-functionapps-demo/tree/main/custom_toolbox',
+    url='https://github.com/felihong/Azure-functionapps-demo',
     license='MIT',
-    packages=['custom_toolbox'],
+    packages=find_packages('toolbox'),
+    package_dir={'': 'toolbox'},
     install_requires=['requests'],
 )
--- toolbox/user_management.py
+++ toolbox/user_management.py
@@ -13,4 +13,3 @@ class userManager:
 if __name__ == "__main__":
     umanager = userManager()
     print(umanager.reverse_name('HONG'))
-
