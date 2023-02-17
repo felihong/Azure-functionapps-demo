b102f85bbd3b340819719f6c04f1094f160f2fef
felihong <echozhu1991@gmail.com>
2023-02-17 15:54:42 +0100

    Update setup.py

--- custom_toolbox/setup.py
+++ custom_toolbox/setup.py
@@ -3,7 +3,6 @@ import setuptools
 
 setuptools.setup(
     name='custom_toolbox',
-    version='0.0.1',
     author='Hong Zhu',
     author_email='zhuhong@microsoft.com',
     description='Example customized package',
