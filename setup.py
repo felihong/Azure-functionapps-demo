9ada740ce4d540414431f7ddc75da24940d71792
felihong <echozhu1991@gmail.com>
2023-02-17 16:31:56 +0100

    Small changes

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 
 setuptools.setup(
-    name='custom-toolbox',
+    name='custom_toolbox',
     version='0.0.1',
     author='Hong Zhu',
     author_email='zhuhong@microsoft.com',
     description='Example customized package',
     url='https://github.com/felihong/Azure-functionapps-demo',
     license='MIT',
-    packages=['custom-toolbox'],
+    packages=['custom_toolbox'],
     install_requires=['requests'],
 )
