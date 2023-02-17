e8d139a2aff1ae8dd82658788da6546832d29fff
felihong <echozhu1991@gmail.com>
2023-02-17 16:24:03 +0100

    Rename package back

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 
 setuptools.setup(
-    name='custom_toolbox',
+    name='toolbox',
     version='0.0.1',
     author='Hong Zhu',
     author_email='zhuhong@microsoft.com',
     description='Example customized package',
     url='https://github.com/felihong/Azure-functionapps-demo/tree/main/custom_toolbox',
     license='MIT',
-    packages=['custom_toolbox'],
+    packages=['toolbox'],
     install_requires=['requests'],
 )
