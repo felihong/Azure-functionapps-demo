d2790c916fbea91f2fe170278ec4992a2bd25c7c
felihong <echozhu1991@gmail.com>
2023-02-17 14:02:52 +0100

    Add example custom package

---
+++ custom_pkg/user_management.py
@@ -0,0 +1,16 @@
+"""
+Toy class for managing user identities with basic example methods.
+"""
+class userManager:
+
+    def __init__(self):
+        pass
+
+    def reverse_name(self, name):
+        return ''.join(reversed(name))
+
+
+if __name__ == "__main__":
+    umanager = userManager()
+    print(umanager.reverse_name('HONG'))
+
