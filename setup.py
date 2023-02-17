662f4ec967d9533ea375fcfc8b4ede243b22b9b8
felihong <echozhu1991@gmail.com>
2023-02-17 16:28:53 +0100

    Update to pkgs and setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 
 setuptools.setup(
-    name='toolbox',
+    name='custom-toolbox',
     version='0.0.1',
     author='Hong Zhu',
     author_email='zhuhong@microsoft.com',
     description='Example customized package',
     url='https://github.com/felihong/Azure-functionapps-demo',
     license='MIT',
-    packages=['toolbox'],
+    packages=['custom-toolbox'],
     install_requires=['requests'],
 )
